473a1cd7e2a3153a1ce87aaf25903435d3c8a1e0
Ayushka Partohap <ayushka.partohap@happyfresh.com>
2021-04-09 22:23:28 +0700

    Assertions cheatsheet

--- solution.test.js
+++ solution.test.js
@@ -3,3 +3,16 @@ const SUT = require("./solution")
 test("My Test", () => {
   expect(SUT.solution()).toBe("hello world")
 })
+
+
+
+// Assertions
+// toBe
+// not.*
+// toEqual
+// toBeTruthy
+// toBeFalsy
+// toBeDefined
+// toBeNull
+// toMatch /rgx/
+// toContain (arr)
